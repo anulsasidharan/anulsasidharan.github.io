e2b1c7d432c0a115395bb5de3f0dac65f344ac3a
Anu L Sasidharan <anulsasidharan@gmail.com>
2025-11-10 10:59:38 -0500

    Add portfolio website

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
