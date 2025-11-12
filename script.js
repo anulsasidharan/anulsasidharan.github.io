54dfc9e2c14785be61f20aa78e953706bab70f67
Anu L Sasidharan <anulsasidharan@gmail.com>
2025-11-11 23:36:57 -0500

    page updates

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
+const scrollElements = document.querySelectorAll(".scroll-animation");
+
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return elementTop <= (window.innerHeight || document.documentElement.clientHeight) - offset;
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("active");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach(el => {
+    if(elementInView(el, 100)) {
+      displayScrollElement(el);
+    }
   });
-  
+};
+
+window.addEventListener("scroll", handleScrollAnimation);
+window.addEventListener("load", handleScrollAnimation);
